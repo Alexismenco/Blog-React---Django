78b3e4edce1bbea44bd1065c3ebb7bf6d6d5fc1d
Alexis Menco <mencoalexis@gmail.com>
2022-11-16 10:44:45 -0300

    Blog React + Django

---
+++ src/containers/pages/Home.jsx
@@ -0,0 +1,19 @@
+import BlogList from "components/blog/BlogList";
+import FullWidthLayout from "hocs/layouts/FullWidthLayout";
+import { connect } from "react-redux";
+
+function Home({
+}){
+
+    return(
+        <FullWidthLayout>
+            Home
+        </FullWidthLayout>
+    )
+}
+
+const mapStateToProps = state =>({
+})
+
+export default connect(mapStateToProps,{
+})(Home)
